bccac1a88b453b94b55e4d5cc5a7fc53d774dc8f
ecomercioumng <72948874+ecomercioumng@users.noreply.github.com>
2020-10-22 20:01:30 -0500

    Create pag.php

---
+++ pag.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Agended: La red que te une con tus Clientes</title>
+	<!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<div class="container"></div>
+	<div class="row">
+		<div class="col s12"><?php include("Include/Cabecera.php"); ?></div>
+	</div>
+	<div class="row">
+		<div class="col s12 m6 yellow">Registrate
+			<div class="row">
+				<form class="col s12">
+					<div class="input-field col s12">
+						<input type="text" class="validate" id="Usuario">
+						<label for="Usuario">Usuario</label>
+					</div>
+					<div class="input-field col s12">
+						<input type="text" class="validate" id="Correo">
+						<label for="Correo">Correo ElectrÃ³nico</label>
+					</div>
+					<div class="input-field col s12">
+						<input type="text" class="validate" id="Password">
+						<label for="Password">ContraseÃ±a</label>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="col s6 m6 green">Inicia SesiÃ³n</div>
+	</div>
+	<div class="row">
+		<div class="col s3">Tiene 3 columnas</div>
+		<div class="col s6 blue">Tiene 6 columnas</div>
+		<div class="col s3">Tiene 3 columnas</div>
+	</div>
+	<!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
